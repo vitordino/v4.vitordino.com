a508e45b5a9696034e9378c089e82f14dcfd257e
vitordino <me@vitordino.com>
2023-09-03 21:46:24 -0300

    :sparkles: grid overlay

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 import windyRadixPlugin from 'windy-radix-palette'
-import { slate, slateDark } from '@radix-ui/colors'
+import { slate, slateDark, tomato, tomatoDark } from '@radix-ui/colors'
+
+export const x = defaultTheme.screens
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [windyRadixPlugin({ colors: { slate, slateDark } })],
-	// darkMode: 'class',
+	plugins: [windyRadixPlugin({ colors: { slate, slateDark, tomato, tomatoDark } })],
 	darkMode: ['class', '[data-theme="dark"]'],
+	theme: { ...defaultTheme, gridTemplateColumns: { '16': 'repeat(16, minmax(0, 1fr))' } },
 } satisfies Config
