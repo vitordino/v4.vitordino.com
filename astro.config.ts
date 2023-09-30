55206bb654be13386cf23db4810317c370a921bb
vitordino <me@vitordino.com>
2023-09-29 22:16:42 -0300

    :sparkles: add more custom markdown components

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import vercelStatic from '@astrojs/vercel/static'
 
 export default defineConfig({
 	integrations: [tailwind(), mdx()],
 	markdown: { shikiConfig: { theme: 'one-dark-pro' } },
+
+	adapter: vercelStatic({ devImageService: 'sharp', imageService: true }),
 })
