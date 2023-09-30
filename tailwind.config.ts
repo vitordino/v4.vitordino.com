4923db106343b67096de5c02eadd5cf63b8d9a26
vitordino <me@vitordino.com>
2023-09-29 23:19:22 -0300

    :lipstick: refine markdown styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,21 +3,24 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 import windyRadixPlugin from 'windy-radix-palette'
 import { slate, slateDark, tomato, tomatoDark } from '@radix-ui/colors'
 
-export const x = defaultTheme.screens
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	plugins: [windyRadixPlugin({ colors: { slate, slateDark, tomato, tomatoDark } })],
 	darkMode: ['class', '[data-theme="dark"]'],
 	theme: {
 		...defaultTheme,
-		gridTemplateColumns: { '16': 'repeat(16, minmax(0, 1fr))' },
-		gridColumn: {
-			...defaultTheme.gridColumn,
-			'span-13': 'span 13 / span 13',
-			'span-14': 'span 14 / span 14',
-			'span-15': 'span 15 / span 15',
-			'span-16': 'span 16 / span 16',
+		extend: {
+			fontFamily: {
+				serif: ['Tiempos Text', ...defaultTheme.fontFamily.serif],
+			},
+			gridTemplateColumns: { '16': 'repeat(16, minmax(0, 1fr))' },
+			gridColumn: {
+				...defaultTheme.gridColumn,
+				'span-13': 'span 13 / span 13',
+				'span-14': 'span 14 / span 14',
+				'span-15': 'span 15 / span 15',
+				'span-16': 'span 16 / span 16',
+			},
 		},
 	},
 	safelist: [
