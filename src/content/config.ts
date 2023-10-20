2451b6f3352addbcdf5ca892b394ba62d9c07da7
vitordino <me@vitordino.com>
2023-10-19 23:54:26 -0300

    :memo: add experience content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,15 @@
 import { z, defineCollection } from 'astro:content'
 
+const experience = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		dateRange: z.string(),
+		to: z.string(),
+		description: z.string(),
+	}),
+})
+
 const writing = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -14,4 +24,4 @@ const writing = defineCollection({
 })
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
-export const collections = { writing }
+export const collections = { writing, experience }
