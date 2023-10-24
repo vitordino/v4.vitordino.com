4799f2d23f14bdc55eda3bc262f32c678b97320f
vitordino <me@vitordino.com>
2023-10-24 11:14:09 -0300

    :chart_with_upwards_trend: vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import vercelStatic from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
 	integrations: [tailwind(), mdx()],
 	markdown: { shikiConfig: { theme: 'one-dark-pro' } },
-
-	adapter: vercelStatic({ devImageService: 'sharp', imageService: true }),
+	output: 'hybrid',
+	adapter: vercel({ devImageService: 'sharp', imageService: true, webAnalytics: { enabled: true } }),
 })
